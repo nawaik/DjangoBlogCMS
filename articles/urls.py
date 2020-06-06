b56dcfe9476bc1cd1ce7126638e5df318cd6443c
nawaik <walikorn@protonmail.com>
2020-06-06 22:32:13 +0200

    Crud artikels af en footer naar naar beneden

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import ShowArticlesView, CreateArticleView, DetailArticleView
+from .views import ShowArticlesView, CreateArticleView, DetailArticleView, ShowOwnArticlesView, UpdateArticleView, DeleteArticleView
 
 urlpatterns = [
     path('', ShowArticlesView.as_view(), name='homepage'),
+    path('artikels', ShowOwnArticlesView.as_view(), name="artikels"),
     path('artikel/<int:pk>', DetailArticleView.as_view(), name="artikeldetail"),
     path('artikelaanmaken/', CreateArticleView.as_view(), name='artikelaanmaken'),
+    path('artikel/bewerken/<int:pk>', UpdateArticleView.as_view(), name="artikelbewerken"),
+    path('artikel/verwijder/<int:pk>', DeleteArticleView.as_view(), name="artikelverwijderen"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 from django.views.generic import DetailView
+from django.urls import reverse_lazy
 
 from .forms import CreateArticleForm
 from .models import Articles
@@ -15,6 +16,14 @@ class DetailArticleView(DetailView):
     model = Articles
     template_name = 'artikelen/articledetail.html'
 
+class ShowOwnArticlesView(ListView):
+    model = Articles
+    paginate_by = 40
+    template_name = 'artikelen/articlelist.html'
+
+    def get_queryset(self):
+        return Articles.objects.filter(author=self.request.user)
+
 class CreateArticleView(CreateView):
     form_class = CreateArticleForm
     template_name = 'artikelen/createarticle.html'
@@ -23,3 +32,13 @@ class CreateArticleView(CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+class UpdateArticleView(UpdateView):
+    model = Articles
+    fields = ['title', 'text', 'meta_title', 'meta_description']
+    template_name = 'artikelen/updatearticle.html'
+
+class DeleteArticleView(DeleteView):
+    model = Articles
+    context_object_name = 'article'
+    success_url = reverse_lazy('artikels')
+    template_name = 'artikelen/deletearticle.html'
